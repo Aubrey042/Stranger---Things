1494901206d88f0028599535efb48e9868475b86
Aubrey042 <127706254+Aubrey042@users.noreply.github.com>
2023-04-12 15:06:04 -0400

    befor installing bundle js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navbar, Login } from ".";
+
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello to you! From App.js</h1>
+      <Navbar />
+      <Login />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>React Navbar.js</h1>
+    </div>
+  );
+};
+
+export default Navbar;
